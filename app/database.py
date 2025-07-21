c2789e90dfaa2a442c211578264d13643f68b514
Juel-Moria <justicenzefuihu@gmail.com>
2025-07-20 23:27:50 -0700

    add alembic and modify files in app folder

--- app/database.py
+++ app/database.py
@@ -1,14 +1,10 @@
 from sqlalchemy import create_engine
-from config import setting
-from sqlalchemy.ext.declarative import declarative_base
+from .config import setting
 from sqlalchemy.orm import sessionmaker
-
 
 DATABASE_URL = f"postgresql+psycopg2://{setting.database_username}:{setting.database_password}@{setting.database_hostname}:{setting.database_port}/{setting.database_name}"
 
 engine = create_engine(DATABASE_URL)
-
-Base =  declarative_base()
 
 sessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
--- app/model.py
+++ app/model.py
@@ -1,17 +1,18 @@
 from datetime import datetime
-from sqlalchemy import ForeignKey, UniqueConstraint
-from enum import Enum
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import (
-    Mapped, mapped_column, DeclarativeBase, relationship
+    Mapped, mapped_column, relationship, DeclarativeBase
 )
 from typing import List
-from .database import Base
+
+class Base(DeclarativeBase):
+    id: Mapped[int] = mapped_column(primary_key=True)
+
 
 
 class UserDB(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(unique=True)
     link: Mapped[str] = mapped_column(unique=True)
     created_at: Mapped[datetime] = mapped_column(
@@ -19,20 +20,29 @@ class UserDB(Base):
         onupdate=datetime.utcnow
         )
     
-    status: Mapped[List['StatusDB']] = relationship(back_populates='user')
+    statuses: Mapped[List["StatusDB"]] = relationship(
+        "StatusDB", back_populates="user", cascade="all, delete"
+    )
     
 
 class StatusDB(Base):
-    __tablename__ = "status"
+    __tablename__ = "statuses"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[int] = mapped_column(ForeignKey(
-        "user.id", ondelete="CASCADE"
+        "users.id", ondelete="CASCADE"
     ), index=True)
+    write_up: Mapped[str | None] = mapped_column(nullable=True)
+    is_upload: Mapped[bool] = mapped_column(default=False)
 
-    user: Mapped[UserDB] = relationship(back_populates="status")
+    user: Mapped[UserDB] = relationship(back_populates="statuses")
+    images: Mapped[List['ImageDB']] = relationship(back_populates='status', cascade="all, delete")
 
 class ImageDB(Base):
-    __tablename__ = "status"
+    __tablename__ = "images"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    images: Mapped[str] = mapped_column()
+    status_id: Mapped[int] = mapped_column(ForeignKey(
+        "statuses.id", ondelete="CASCADE"
+    ), index=True)
+
+    status: Mapped[StatusDB] = relationship(back_populates="images")
