3c97ae3793cb424b236068de8e012999f78ce4a1
Juel-Moria <justicenzefuihu@gmail.com>
2025-07-22 11:11:08 -0700

    added getting or create user from a postgres db

--- app/main.py
+++ app/main.py
@@ -8,6 +8,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+from sqlalchemy.orm import Session, joinedload
+from sqlalchemy.exc import SQLAlchemyError
+from .database import sessionLocal
+from .model import  UserDB
 
 BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
 PROFILES_DIR = os.path.join(BASE_DIR, "profiles")
@@ -22,6 +26,7 @@ def launch_whatsapp_session(user_id):
     options.add_argument("--profile-directory=Default")
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option('useAutomationExtension', False)
+
     browser = webdriver.Chrome(options=options)
     return browser
 
@@ -48,29 +53,34 @@ def type_text(xpath, value):
         sleep(0.1)
     sleep(2)
 
-def phone_number_login():
+def phone_number_login(user : UserDB):
     select_clickable_element("//div[contains(text(), 'Log in with phone number')]")
     select_clickable_element("//button[.//span[contains(@data-icon, 'chevron')]]")
-    country = 'Nigeria'
+
+    country = user.country
     type_text("//p[contains(@class, 'selectable-text copyable-text')]", country)
     select_clickable_element( "(//button[contains(@aria-label, 'Selected country:')])[1]")
-    phone_number = '9043262304'
+
+    phone_number = user.phone
     type_text("//input[contains(@aria-label, 'phone number')]", phone_number)
     select_clickable_element("(//button[.//div[contains(text(),'Next')]])[1]")
     link_code = get_code()
     print("Link Code:", link_code)
     input("After linking device, press Enter...")
+    sleep(30)
 
-def login_or_restore():
+def login_or_restore(user : UserDB):
     browser.get("https://web.whatsapp.com/")
+
     try:
         select_clickable_element("//button[.//div[contains(text(), 'Continue')]]")
     except TimeoutException:
         pass
+
     try:
         select_clickable_element("(//button[contains(@aria-label,'Status')])[1]")
     except TimeoutException:
-        phone_number_login()
+        phone_number_login(user)
 
 def get_code():
     link_code_element = select_element("(//div[contains(@aria-details, 'device-phone-number-code-screen')])[1]")
@@ -78,29 +88,39 @@ def get_code():
     link_code = link_code_element.get_attribute('data-link-code').replace(',', '').strip()
     return link_code
 
-def send_status_images():
-    select_clickable_element("(//button[contains(@aria-label,'Status')])[2]")
-    input()
-    select_clickable_element("//div[contains(@role, 'application')]//ul//li[.//span[contains(text(), 'video')]]")
-    # text_element = update_status_ul_element[1]
-    # click(image_element)
+def send_status_text(write_ups):
+    if write_ups:
+        select_clickable_element("(//button[contains(@aria-label,'Status')])[2]")
+        select_clickable_element("//div[contains(@role, 'application')]//ul//li[.//span[contains(text(), 'Text')]]")
 
-    statuses = [
-        (r"C:\Users\HP\Desktop\Hotel\Juel\media\photos\2023\11\27\about4.jpg",'room 1'),(r"C:\Users\HP\Desktop\Hotel\Juel\media\photos\2023\11\27\about4.jpg", 'room 2')
-        ]
-    for i, (file_path, caption) in enumerate(statuses):
-        pyautogui.write(file_path)
-        pyautogui.press("enter")
-        sleep(2)
-        type_text("//div[@aria-label='Add a caption']//p[contains(@class,'selectable-text copyable-text')]", caption)
-        if i < len(statuses)-1:
-            select_clickable_element("//button[contains(@title, 'Add file')]")
-    
-    select_clickable_element("//div[contains(@aria-label, 'Send')]")
+        for i, write_up in enumerate(write_ups):
+            type_text("//div[@aria-placeholder='Type a status']//p[contains(@class,'selectable-text copyable-text')]", write_up)
+            select_clickable_element("//div[contains(@aria-label, 'Send')]")
+
+            if i < len(write_ups)-1:
+                select_clickable_element("(//button[contains(@aria-label,'Status')])[2]")
+                select_clickable_element("//div[contains(@role, 'application')]//ul//li[.//span[contains(text(), 'Text')]]")
+        
+def send_status_images(images):
+    if images:
+        select_clickable_element("(//button[contains(@aria-label,'Status')])[2]")
+        select_clickable_element("//div[contains(@role, 'application')]//ul//li[.//span[contains(text(), 'video')]]")
+
+        for i, (image_path, caption) in enumerate(images):
+            pyautogui.write(image_path)
+            pyautogui.press("enter")
+            sleep(2)
+
+            type_text("//div[@aria-label='Add a caption']//p[contains(@class,'selectable-text copyable-text')]", caption)
+
+            if i < len(images)-1:
+                select_clickable_element("//button[contains(@title, 'Add file')]")
+        
+        select_clickable_element("//div[contains(@aria-label, 'Send')]")
 
 def main():
-    print("in the main function")
-    login_or_restore()
+    login_or_restore(user)
+
     try:
         ok_element = select_clickable_element("//button[.//div[contains(text(), 'OK')]]")
         if ok_element:
@@ -108,12 +128,46 @@ def main():
             main()
     except TimeoutException:
         pass
-    send_status_images()
+
+    write_ups = []
+    images = []
+    for status in user.statuses:
+        if not status.is_upload:
+            if status.is_text:
+                write_ups.append(status.write_up)
+            else:
+                images.append((status.images_path, status.write_up))
+
+    send_status_images(images)
+    send_status_text(write_ups)
+    sleep(15)
     browser.quit()
 
+def get_or_create_user(phone: str, country: str, db: Session):
+    try:
+        user = db.query(UserDB).filter_by(phone=phone, country=country).options(
+            joinedload(UserDB.statuses)
+        ).first()
+
+        if not user:
+            user = UserDB(phone=phone, country=country)
+            db.add(user)
+            db.commit()
+            db.refresh(user)
+
+        return user
+
+    except SQLAlchemyError as e:
+        db.rollback()
+        print(f"Error: {e}")
+        return None
+
 if __name__ == "__main__":
-    print("hello world")
-    browser = launch_whatsapp_session('justice_nzefuihu')
+    try:
+        db = sessionLocal()
+        user: UserDB = get_or_create_user(db, phone="9043262304", country="Nigeria")
+    finally:
+        db.close()
+    browser = launch_whatsapp_session(user.id)
     wait = WebDriverWait(browser, timeout=10)
-    print("In the if statement")
     main()
--- app/model.py
+++ app/model.py
@@ -1,24 +1,27 @@
 from datetime import datetime
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, String, UniqueConstraint
+from sqlalchemy.sql import func
 from sqlalchemy.orm import (
     Mapped, mapped_column, relationship, DeclarativeBase
 )
 from typing import List
 
 class Base(DeclarativeBase):
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
 
 
 
 class UserDB(Base):
     __tablename__ = "users"
+    __table_args__ = (
+        UniqueConstraint("phone", 'country', name="uq_user_phone_country"),
+    )
 
-    email: Mapped[str] = mapped_column(unique=True)
-    link: Mapped[str] = mapped_column(unique=True)
+    phone: Mapped[str] = mapped_column(String(20), nullable=False)
+    country: Mapped[str] = mapped_column(String(50), nullable=False)
     created_at: Mapped[datetime] = mapped_column(
-        default=datetime.utcnow,
-        onupdate=datetime.utcnow
-        )
+        default=func.now()
+    )
     
     statuses: Mapped[List["StatusDB"]] = relationship(
         "StatusDB", back_populates="user", cascade="all, delete"
@@ -33,16 +36,11 @@ class StatusDB(Base):
     ), index=True)
     write_up: Mapped[str | None] = mapped_column(nullable=True)
     is_upload: Mapped[bool] = mapped_column(default=False)
+    is_text: Mapped[bool] = mapped_column(default=False)
+    images_path: Mapped[str] = mapped_column(String(255))
+    created_at: Mapped[datetime] = mapped_column(
+        default=func.now()
+    )
+    sequence: Mapped[int] = mapped_column()
 
     user: Mapped[UserDB] = relationship(back_populates="statuses")
-    images: Mapped[List['ImageDB']] = relationship(back_populates='status', cascade="all, delete")
-
-class ImageDB(Base):
-    __tablename__ = "images"
-
-    images: Mapped[str] = mapped_column()
-    status_id: Mapped[int] = mapped_column(ForeignKey(
-        "statuses.id", ondelete="CASCADE"
-    ), index=True)
-
-    status: Mapped[StatusDB] = relationship(back_populates="images")
